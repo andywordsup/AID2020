f12b7c572bb50420779279c2b3c0ce9a035fc619
Andy <andywordsup@gmail.com>
2020-10-14 13:44:58 +0800

    change something

--- blog_pro/client/flask_client.py
+++ blog_pro/client/flask_client.py
@@ -13,28 +13,68 @@ app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    # 首页
+   
     return send_file('templates/index.html')
 
 
 @app.route('/index2')
 def index2():
-    # 首页
+   
     return send_file('templates/index2.html')
 
 @app.route('/login')
 def login():
-    # 登录
+    
     return send_file('templates/login.html')
 
 
 @app.route('/register')
 def register():
-    # 注册
+   
     return send_file('templates/register.html')
 
 
+@app.route('/<username>/info')
+def info(username):
+    
+    return send_file('templates/about.html')
 
+
+@app.route('/<username>/change_info')
+def change_info(username):
+   
+    return send_file('templates/change_info.html')
+
+
+
+@app.route('/<username>/topic/release')
+def topic_release(username):
+   
+    return send_file('templates/release.html')
+
+
+@app.route('/<username>/topics')
+def topics(username):
+   
+    return send_file('templates/list.html')
+
+
+@app.route('/<username>/topics/detail/<t_id>')
+def topics_detail(username, t_id):
+    
+    return send_file('templates/detail.html')
+
+@app.route('/pp')
+def list():
+    
+    return send_file('templates/pp.html')
+ 
+
+
+@app.route('/<username>/change_topics/detail/<t_id>')
+def change_topics(username,t_id):
+   
+    return send_file('templates/change_topics.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
